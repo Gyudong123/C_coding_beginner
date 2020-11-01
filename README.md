a76f3286329e51ba997df49518772c22f389f9dc
Gyudong123 <65889044+Gyudong123@users.noreply.github.com>
2020-11-01 17:51:52 +0900

    20.11.1
    studied data type

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # C_coding
  Studying_C_base
+Let's study C!
+It is for understanding basic words of computer science.(Ex ASCII? IEEE 754? interpreter?)
+
+Negative numbers are the remuneration of two.
+Use %.10lf to print very small decimal number(+.5e-09)
+IEEE 754 includes 15 significant figures(64 bit)
+SPACE ' "'s ASCII is 32!
--- test.c
+++ test.c
@@ -2,16 +2,12 @@
 
 int main()
 {
-    unsigned num,day,hour,min,sec;
-    printf("몇 살까지 살고 싶습니까? : ");
-    scanf("%u",&num);
-    day = (num ) * 365;
-    hour = day * 24;
-    min = hour * 60;
-    sec = min * 60;
-    printf("%d,%u,%u,%u",day,hour,min,sec);
+char ch;
 
+printf("input character : ");
+scanf("%c", &ch);
 
-    return 0;
+printf("character %c ASCCI : %x \n", ch,ch);
+return 0;
 
 }
